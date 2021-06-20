25bffc635b4873de98b5b6ba2d17e8c320dfb944
Claudia Canamas-Donnelly <cdonnelly@efclocal.com>
2021-06-20 19:24:40 -0400

    found max increase in profits

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,28 +4,42 @@ filename = 'C:/Users/cdonnelly.EFCLOCAL/Desktop/Bootcamp/python-challenge/PyBank
 
 numMonths = 0
 netTotal = 0
+# list of month to month changes
 changes = []
 changetemp = 0
 avgChange = 0
+# counter lets us know what row we're on
 counter = 0
+
+# max  increase in profits
+maxp = 0
+maxp_month = ""
+
 with open(filename, 'r') as file:
     reader = csv.reader(file)
     for row in reader:
         counter += 1
+        # Ignore header row
         if counter > 1:
             numMonths += 1
             netTotal += int(row[1])
+        # only start computing differences from row 3
         if counter > 2:
             changetemp = (int(row[1]) - int(prev[1]))
             changes.append(changetemp)
-            print(changetemp)
+            if (changetemp > maxp):
+                maxp = changetemp
+                maxp_month = row[0]
+            #print(changetemp)
         prev = row
 
-avgChange = sum(changes) / len(changes)
+avgChange = round((sum(changes) / len(changes)),2)
 
+# Write financial analysis text file
 anal = open("C:/Users/cdonnelly.EFCLOCAL/Desktop/Bootcamp/python-challenge/PyBank/analysis/financial_analysis.txt", "w")
 anal.write("Financial Analysis \n_________________________________________________________ \n")
 anal.write("\nTotal Months: " + str(numMonths))
 anal.write("\nTotal: $" + str(netTotal))
 anal.write("\nAverage Change: $" + str(avgChange))
+anal.write("\nGreatest Increase in Profits: "+ maxp_month + " ($" + str(maxp) + ")")
 anal.close()
