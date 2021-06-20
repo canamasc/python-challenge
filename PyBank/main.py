5d2e8aa290c8f4643da8b274d6d5de9484bd51db
Claudia Canamas-Donnelly <cdonnelly@efclocal.com>
2021-06-20 18:44:47 -0400

    first draft, opens csv file, computes first two metrics, and writes to .txt

---
+++ PyBank/main.py
@@ -0,0 +1,25 @@
+import csv
+
+filename = 'C:/Users/cdonnelly.EFCLOCAL/Desktop/Bootcamp/python-challenge/PyBank/Resources/budget_data.csv'
+
+numMonths = 0
+netTotal = 0
+avgChange = 0
+counter = 0
+with open(filename, 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        counter += 1
+        if counter > 1:
+            numMonths += 1
+            netTotal += int(row[1])
+
+
+avgChange = netTotal / numMonths
+
+anal = open("C:/Users/cdonnelly.EFCLOCAL/Desktop/Bootcamp/python-challenge/PyBank/analysis/financial_analysis.txt", "w")
+anal.write("Financial Analysis \n_________________________________________________________ \n")
+anal.write("\nTotal Months: " + str(numMonths))
+anal.write("\nTotal: $" + str(netTotal))
+anal.write("\nAverage Change: $" + str(avgChange))
+anal.close()
