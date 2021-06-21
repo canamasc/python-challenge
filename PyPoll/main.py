2cee8648c5c4e94391774b2f3e94caa19a84b513
Claudia Canamas-Donnelly <cdonnelly@efclocal.com>
2021-06-21 09:07:06 -0400

    counts total votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,17 @@
 import csv
 
 filename = 'C:/Users/cdonnelly.EFCLOCAL/Desktop/Bootcamp/python-challenge/PyPoll/Resources/election_data.csv'
+
+totalVotes = 0
+
+with open(filename, 'r') as file:
+    reader = csv.reader(file)
+    # Store header row
+    header = next(reader)
+    for row in reader:
+        if row != header:
+            totalVotes += 1
+
+# Print Results to terminal
+print("Election Results \n_________________________________________________________ \n")
+print("\nTotal Votes: " + str(totalVotes) + "\n_________________________________________________________ \n")
